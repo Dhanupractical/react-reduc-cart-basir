c99ba0de4e41d2719af75084b9b14f55d7074093
DHANANJAYA K R <dhananjayakrd@gmail.com>
2021-12-17 01:40:38 +0530

    filter-component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,67 @@
 import React from 'react'
+import Filter from './components/Filter';
+import Products from './components/Products';
+import data from './data.json'
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      products: data.products,
+      size:"",
+      sort:"",
+    };
+  }
+  sortProducts=(e)=>{
+   console.log(e.target.value);
+   const sort =e.target.value;
+   this.setState((state)=>({
+     sort : sort,
+     products : this.state.products.slice().sort((a,b)=>( // dhanu why slice inbetween 
+       sort ==='lowest' ?
+       ((a.price > b.price)? 1 : -1):
+       sort === 'highest'?
+       ((a.price < b.price)? 1: -1):  // dhanu  why 1 and -1
+        a._id >b._id ? 1 : -1
+     ))}))
+  }
+  filterProducts=(e)=>{
+   console.log(e.target.value);
+   if (e.target.value === "") {
+     this.setState({size: e.target.value, products:data.products});
+   } else {
+    this.setState({
+      size: e.target.value,
+      products:data.products.filter(
+        (product) => product.availableSizes.indexOf(e.target.value) >= 0
+      ),
+    });
+   }
+
+  };
+  render(){
+    return (
+      <div className="grid-container">
+       <header>
+         <a href="/">React Shopping Cart</a>
+       </header>
+       <main>
+         <div className="content">
+           <div className="main"> 
+              <Filter count={this.state.products.length}
+                      size={this.state.size}
+                      sort={this.state.sort}
+                      filterProducts={this.filterProducts}
+                      sortProducts={this.sortProducts}></Filter>
+              <Products products={this.state.products}></Products>
+           </div>
+           <div className="sidebar">Cart Items</div>
+         </div>
+       </main>
+       <footer>All right is reserved.</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
